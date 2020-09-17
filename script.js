ba4052a9caab95183cca8413b8e52d63f8d228f7
Kenneth Kopelson <kennethkopelson@gmail.com>
2020-09-17 12:52:47 -0700

    Refactored code to be less repetitive and tested

--- script.js
+++ script.js
@@ -81,27 +81,27 @@ function getRandomChar() {
   let i = 0;
     //this loops through untill a valid option is selected.
     do {
-      //these if statments check to see if the randomize option is valid and then will run the assoicated function.
+      //these if statments check to see if the randomize option is valid and then will run the getRandom function.
       if (getRandomChar === 0 && useLowercase.checked){
         //this adds 1 to i to stop the do loop.
         i++;
-        //this returns the random lowercase letter by runing this function.
-        return getRandomLowerLetter();
+        //this returns the random lowercase letter by calling this function.
+        return getRandom(0);
       } else if (getRandomChar === 1 && useUppercase.checked){
         //this adds 1 to i to stop the do loop.
         i++;
-        //this returns the random uppercase letter by runing this function.
-        return getRandomUpperLetter();
+        //this returns the random uppercase letter by calling this function.
+        return getRandom(1);
       } else if (getRandomChar === 2 && useNumeric.checked){
         //this adds 1 to i to stop the do loop.
         i++;
-        //this returns the random number by runing this function.
-        return getRandomNum();
+        //this returns the random number by calling this function.
+        return getRandom(2);
       } else if (getRandomChar === 3 && useSpecial.checked) {
         //this adds 1 to i to stop the do loop.
         i++
-        //this returns the random special character by runing this function.
-        return getSpecialChar();
+        //this returns the random special character by calling this function.
+        return getRandom(3);
         //this checks if no options are checked and returns nothing.
       } else if (useLowercase.checked === false && useUppercase.checked === false && useNumeric.checked === false && useSpecial.checked === false) {
         //this adds 1 to i to stop the do loop.
@@ -115,52 +115,19 @@ function getRandomChar() {
     } while (i = 1);
 };
 
-//this function will return a random lowercase letter.
-function getRandomLowerLetter() {
-  //this initalizes an array of lowercase letters.
-  const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  //this assigns randomCharacters a random number 0 - 25.
-  let randomCharacter = Math.floor(Math.random()*26);
-  //this assigns randomCharacters an index of the alphebet array.
-  randomCharacter = alphabet[randomCharacter];
-  //this returns a random lower case letter back to the getRandomChar function.
-  return randomCharacter;
-};
-
-//this function will return a random uppercase letter.
-function getRandomUpperLetter() {
-  //this initalizes an array of uppercase letters.
-  const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  //this assigns randomCharacters a random number 0 - 25.
-  let randomCharacter = Math.floor(Math.random()*26);
-  //this assigns randomCharacters an index of the alphabet array.
-  randomCharacter = alphabet[randomCharacter];
-  //this returns an uppercase letter to the getRandomChar function.
-  return randomCharacter;
-};
-
-//this function will return a special character.
-function getSpecialChar() {
-  //this initalizes an array of special characters.
-  const special = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
-  //this assigns randomCharacter a random number 0 - 29.
-  let randomCharacter = Math.floor(Math.random()*30);
-  //this assigns randomCharacter an idex of the special array.
-  randomCharacter = special[randomCharacter];
-  //this returns a special character to the getRandomChar function.
-  return randomCharacter;
-};
-
-//this function will return a random integer from 0 - 9.
-function getRandomNum() {
-  //this initalizes an array of integers from 0 - 9.
+//this function holds different character types in arrays and takes in a parameter that selects which type of characters are needed and randomly chooses an index inside that array.
+function getRandom(index) {
+  //this initalizes the different character arrays, lowercase a - z, uppercase A - Z, integers 0 - 9, and special characters. 
+  const lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  const upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  //this assigns randomNum a random number 0 - 9.
-  let randomNum = Math.floor(Math.random()*10);
-  //this assigns randomNum to an index of the numbers array. *note this should be the same as the random number that it was given*
-  randomNum = numbers[randomNum];
-  //this returns a random integer that ranges from 0 - 9 back to the getRandomChar function.
-  return randomNum;
-};
-
-
+  const special = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+  //this initalizes an array to store the different character arrays.
+  let array = [lower, upper, numbers, special];
+  //this assigns randomArrayIndex a random number that matches the function argument array's length.
+  let randomArrayIndex = Math.floor(Math.random()*array[index].length);
+  //this assigns randomArrayIndex a random index of the selected array of characters.
+  randomArrayIndex = array[index][randomArrayIndex];
+  //this returns the random index back to the getRandomChar function. 
+  return randomArrayIndex;
+}
